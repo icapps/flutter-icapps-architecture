4df1dde41ee4db4c8e75e0954115c7a14b758b89
Nicola Verbeeck <nicola@chimerapps.com>
2021-04-01 11:29:54 +0200

    - Add environment util to check if we are in test (with tree shaking!) - Added fade in route - Added script to test coverage locally

---
+++ lib/src/route/fade_in_route.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+import 'package:icapps_architecture/src/util/environment_util.dart';
+
+class FadeInRoute<T> extends PageRouteBuilder<T> {
+  final Widget child;
+  final Curve curve;
+  final Duration duration;
+
+  FadeInRoute({
+    required this.child,
+    this.curve = Curves.linear,
+    RouteSettings? settings,
+    this.duration = const Duration(milliseconds: 300),
+  }) : super(
+          pageBuilder: (context, animation, secondaryAnimation) => child,
+          settings: settings,
+          transitionDuration: isInTest ? const Duration(seconds: 0) : duration,
+          transitionsBuilder: (context, animation, secondaryAnimation, child) =>
+              FadeTransition(
+            opacity: animation,
+            child: child,
+          ),
+        );
+}
