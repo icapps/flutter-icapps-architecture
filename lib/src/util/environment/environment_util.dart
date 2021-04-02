a71557075f6fa5ce1aaf106dfb681f8095a31f35
Nicola Verbeeck <nicola@chimerapps.com>
2021-04-02 11:56:31 +0200

    Add constants for release debug and profile modes.
    These are constants to allow tree shaking to work!

--- lib/src/util/environment/environment_util.dart
+++ lib/src/util/environment/environment_util.dart
@@ -4,3 +4,12 @@ import 'impl/environment_util_stub.dart'
 
 /// Returns true if the code is currently being execute by unit tests
 bool get isInTest => !kReleaseMode && runInTest;
+
+/// Returns true if the current code is executing in release mode
+const bool isInRelease = kReleaseMode;
+
+/// Returns true if the current code is executing in debug mode
+const bool isInDebug = kDebugMode;
+
+/// Returns true if the current code is executing in profile mode
+const bool isInProfile = kProfileMode;
