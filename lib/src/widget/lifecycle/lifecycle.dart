12817426174d3103bce959e52b92d999523d8498
rafwillems <raf.willems@icapps.com>
2022-03-01 14:35:03 +0100

    add LifecycleWidget

---
+++ lib/src/widget/lifecycle/lifecycle.dart
@@ -0,0 +1,56 @@
+import 'package:flutter/material.dart';
+
+class LifecycleWidget extends StatefulWidget {
+  final Widget child;
+  final VoidCallback? onResume;
+  final VoidCallback? onPause;
+  final VoidCallback? onDetached;
+  final VoidCallback? onInactive;
+
+  const LifecycleWidget({
+    required this.child,
+    this.onResume,
+    this.onPause,
+    this.onDetached,
+    this.onInactive,
+  });
+
+  @override
+  _LifecycleWidgetState createState() => _LifecycleWidgetState();
+}
+
+class _LifecycleWidgetState extends State<LifecycleWidget>
+    with WidgetsBindingObserver {
+  @override
+  void initState() {
+    super.initState();
+    WidgetsBinding.instance?.addObserver(this);
+  }
+
+  @override
+  Widget build(BuildContext context) => widget.child;
+
+  @override
+  void dispose() {
+    WidgetsBinding.instance?.removeObserver(this);
+    super.dispose();
+  }
+
+  @override
+  void didChangeAppLifecycleState(AppLifecycleState state) {
+    switch (state) {
+      case AppLifecycleState.resumed:
+        widget.onResume?.call();
+        break;
+      case AppLifecycleState.paused:
+        widget.onPause?.call();
+        break;
+      case AppLifecycleState.detached:
+        widget.onDetached?.call();
+        break;
+      case AppLifecycleState.inactive:
+        widget.onInactive?.call();
+        break;
+    }
+  }
+}
